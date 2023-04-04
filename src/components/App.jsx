88bcdcd683481d2d3186fafe12325b63dc27b168
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-04 15:59:44 +0300

    -

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
+// import { Filter } from './Filter/Filter';
 import { Layout } from './Layout/Layout';
 
 // const initialContacts = [
